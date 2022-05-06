ae2328a4909b81e7df6b10f203f7d5c21d486596
Gustavo Henrique Pereira da Silva <ghguga123@gmail.com>
2022-05-06 00:19:44 -0300

    Inativando botão "Feedback"
    Inativando botão de feedback quando o campo não estiver preenchido.

--- web/src/components/WidgetForm/ScreenshotButton.tsx
+++ web/src/components/WidgetForm/ScreenshotButton.tsx
@@ -4,8 +4,8 @@ import { useState } from "react";
 import { Loading } from "../Loading";
 
 interface ScreenshotButtonProps {
-  screenshot: string;
-  onScreenshotTook: (screenshot: string) => void;
+  screenshot: string | null;
+  onScreenshotTook: (screenshot: string | null) => void;
 }
 
 export function ScreenshotButton({ screenshot, onScreenshotTook }: ScreenshotButtonProps) {
@@ -26,8 +26,11 @@ export function ScreenshotButton({ screenshot, onScreenshotTook }: ScreenshotBut
       <button
         type="button"
         className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+        onClick={() => onScreenshotTook(null)}
         style={{
           backgroundImage: `url(${screenshot})`,
+          backgroundPosition: 'right bottom',
+          backgroundSize: 100,
         }}
       >
         <Trash weight="fill" />
