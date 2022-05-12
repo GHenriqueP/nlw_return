7201d7b355ac2625530b62f297362ebc4b8964f7
Gustavo Henrique Pereira da Silva <ghguga123@gmail.com>
2022-05-12 20:53:45 -0300

    Criando Server em Node
    Primeiro envio de arquivos, api FeedGet.

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "fd0404805ab120",
+    pass: "e8aef9ee0eba07"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Diego Fernandes <ghguga123@gmail.com>',
+      subject,
+      html: body,
+    });
+  };
+}
